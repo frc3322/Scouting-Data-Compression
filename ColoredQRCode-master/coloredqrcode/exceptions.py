795718d1280ca2e1ddb7d50d53d73eacd77df08d
DarkEden-Coding <darkedenc9@gmail.com>
2025-11-14 14:20:05 -0500

    remove colored QR code dependency, remove unused ColoredQRCode library

--- ColoredQRCode-master/coloredqrcode/exceptions.py
+++
@@ -1,7 +0,0 @@
-"""
-Custom exceptions for the coloredqrcode package.
-"""
-
-class QRCodeDataTooLongError(ValueError):
-    """Exception raised when the input data exceeds the maximum supported QR code length."""
-    pass
--- ColoredQRCode-master/tests/internal/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Unit test configuration for internal tests."""
-from coloredqrcode.exceptions import QRCodeDataTooLongError
-
-__all__ = ['QRCodeDataTooLongError']
